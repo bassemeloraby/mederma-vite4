79df3a570b6022ddca4c3fc8bd49771dd1604c58
bassem <basem12683@gmail.com>
2024-05-02 03:30:58 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { About } from "./pages";
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!
-  </h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;

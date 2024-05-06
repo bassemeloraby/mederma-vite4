48ac2a38ac4c2dc6cbc5cc22664a0acfadc43855
bassem <basem12683@gmail.com>
2024-05-06 06:29:21 +0300

    vitosolist

--- src/components/ProductVitosoList.jsx
+++ src/components/ProductVitosoList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Virtuoso } from "react-virtuoso";
+import noPhoto from "../assets/noPhoto.jpg";
+import { Link } from "react-router-dom";
 
 const ProductVitosoList = ({ items }) => {
   return (
@@ -11,26 +13,26 @@ const ProductVitosoList = ({ items }) => {
         itemContent={(index, drug) => {
           const { TradeName, PublicPrice, ScientificName } = drug;
           return (
-            <div
-              style={{
-                background: index % 2 === 0 ? "#ffbb00" : "#ffcc99",
-                color: "#333",
-                padding: "10px",
-                fontSize: "16px",
-                fontFamily: "Arial, sans-serif",
-                border: "1px solid #ccc",
-                borderRadius: "5px",
-                margin: "5px 0",
-              }}
-              className=""
+            <Link
+              key={drug._id}
+              to={`/drugs/${drug._id}`}
+              className="p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap  bg-base-100 shadow-xl hover:shadow-2xl duration-300 group"
             >
-              <div>
-                <h6>{TradeName} </h6>
-                <h6>{ScientificName} </h6>
-                <h6>{PublicPrice} SR </h6>
+              <img
+                src={noPhoto}
+                alt={TradeName}
+                className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover group-hover:scale-105 transition duration-300"
+              />
+              <div className="ml-0 sm:ml-16">
+                <h3 className="capitalize font-medium text-lg">{TradeName}</h3>
+                <h4 className="capitalize text-md text-neutral-content">
+                  {ScientificName}
+                </h4>
               </div>
-              
-            </div>
+              <p className="font-medium ml-0 sm:ml-auto text-lg">
+                {PublicPrice}
+              </p>
+            </Link>
           );
         }}
       />

19aafbcffa82eb2ba091856ded0077cbe97d9848
bassem <basem12683@gmail.com>
2024-05-07 23:01:30 +0300

    clean1

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { Form, useLoaderData, Link } from "react-router-dom";
-import FormInput from "./form/FormInput";
-import FormSelect from "./form/FormSelect";
+import React from "react";
+
 import FormCheckbox from "./form/FormCheckbox";
 import FormDtalistInput from "./form/FormDtalistInput";
-// import FormRange from "./form/FormRange";
-// import FormCheckbox from "./form/FormCheckbox";
+
 const Filters = ({
   contents,
   scientificName,
@@ -15,16 +12,10 @@ const Filters = ({
   wasfatyFilter,
   filterHandelr,
 }) => {
-  // console.log("Filters", contents);
-  // console.log(scientificName);
-  // const { ScientificName ,wasfaty} = params;
-
-  // const resetHandler = () => {
-  //   setScientificNameFilter("");
-  // };
-
+  
   return (
     <div className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* ScientificName  */}
       <FormDtalistInput
         name="ScientificName"
         label="select Scientific Name"
@@ -32,7 +23,6 @@ const Filters = ({
         size="input-sm"
         listDB={scientificName}
         defaultValue={scientificNameFilter}
-        // onChange={(e) => setScientificNameFilter(e.target.value)}
         onInput={(e) => setScientificNameFilter(e.target.value)}
       />
       {/* wasfaty */}
@@ -40,15 +30,8 @@ const Filters = ({
         name="wasfaty"
         label="wasfaty"
         size="checkbox-sm"
-        //  defaultValue={wasfaty}
         onChange={() => setWasfatyFilter(!wasfatyFilter)}
       />
-      {/* BUTTONS <button type="submit" className="btn btn-primary btn-sm">
-        search
-      </button>{" "}
-      <button onClick={resetHandler} className="btn btn-accent btn-sm">
-        reset
-      </button>*/}
       <button onClick={filterHandelr} className="btn btn-accent btn-sm">
         search
       </button>

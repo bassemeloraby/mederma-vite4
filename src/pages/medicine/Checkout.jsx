b99fb76fb861e09bb97c637077015f0cdc8ade71
bassem <basem12683@gmail.com>
2024-05-24 17:08:21 +0300

    Checkout

--- src/pages/medicine/Checkout.jsx
+++ src/pages/medicine/Checkout.jsx
@@ -17,6 +17,7 @@ const Checkout = () => {
   const { lists } = useLoaderData();
   // const [descriptionFilter, setDescriptionFilter] = useState("");
   const [items, setItems] = useState(lists);
+  const [listsCount, setlistsCount] = useState(lists.length);
 
   console.log(items);
 
@@ -32,24 +33,43 @@ const Checkout = () => {
     const response = await customFetch.delete(`${url}/${id}`);
     setItems(items.filter((list) => list._id !== id));
     console.log(items);
+    setlistsCount(listsCount -1)
   };
   return (
     <Fragment>
       <SectionTitle text="Checkout" />
-
-      <div>
-        {items.map((li, i) => (
-          <div key={i} className="">
-            <Link>{li.Description}</Link>
-
-            <button
-              onClick={() => deleteSpecialAr(li._id)}
-              className="btn btn-accent btn-sm me-2"
-            >
-              delete
-            </button>
-          </div>
-        ))}
+      <div className="mt-8">
+        <h4 className="mb-4 capitalize">total lists : {listsCount}</h4>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((li, i) => {
+                const { Description } = li;
+                return (
+                  <tr key={i}>
+                    <td>{Description}</td>
+                    <td>
+                      {" "}
+                      <button
+                        onClick={() => deleteSpecialAr(li._id)}
+                        className="btn btn-accent btn-sm me-2"
+                      >
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Fragment>
   );
